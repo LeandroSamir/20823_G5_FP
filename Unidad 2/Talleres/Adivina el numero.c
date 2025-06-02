7b7b3ee25358e19165eb6e23b499a1bb47d60cde
Leandro Zamir Safla Tenorio <zamir_tenorio-2002@hotmail.com>
2025-06-02 08:31:04 -0500

    Update Adivina el numero.c

--- Unidad 2/Talleres/Adivina el numero.c
+++ Unidad 2/Talleres/Adivina el numero.c
@@ -15,14 +15,14 @@ int main() {
     srand(time(NULL));
     numeroSecreto = rand() % 100 + 1;
 
-    printf("Adivina el número entre 1 y 100. Tienes %d intentos.\n", MAX_INTENTOS);
+    printf("Adivina el numero entre 1 y 100. Tienes %d intentos.\n", MAX_INTENTOS);
 
     ingresarDatos(intentos, &adivinado, numeroSecreto, &intentosRealizados);
 
     if (adivinado) {
-        printf("¡Correcto! Adivinaste el número durante los intentos.\n");
+        printf("¡Correcto! Adivinaste el numero durante los intentos.\n");
     } else {
-        printf("No lograste adivinar. El número era: %d\n", numeroSecreto);
+        printf("No lograste adivinar. El numero era: %d\n", numeroSecreto);
     }
 
     imprimirIntentos(intentos, intentosRealizados);
@@ -43,9 +43,9 @@ void ingresarDatos(int intentos[], int *adivinado, int numeroSecreto, int *inten
             printf("¡Correcto!\n");
             break;
         } else if (intento < numeroSecreto) {
-            printf("El número es más alto.\n");
+            printf("El numero es más alto.\n");
         } else {
-            printf("El número es más bajo.\n");
+            printf("El numero es más bajo.\n");
         }
     }
 }
