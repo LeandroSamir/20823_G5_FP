c8861c4aeab1a978e0ab76c1462e289bb59e0e13
MarcoJoelChuquisalaGuanoluisa <mjchuquisala@espe.edu.ec>
2025-06-16 08:40:54 -0500

    Update Ejercicio1.c

--- Unidad 2/Deberes/Deber 2/Ejercicio1.c
+++ Unidad 2/Deberes/Deber 2/Ejercicio1.c
@@ -3,22 +3,21 @@
 int main() {
     int n;
 
-    // Solicitar el tamaño de la matriz cuadrada
-    printf("Introduce el tamaño de la matriz (n x n): ");
+    // RF1: Solicitar el tamaño de la matriz cuadrada
+    printf("Introduce el tamano de la matriz (n x n): ");
     scanf("%d", &n);
 
     int matriz[n][n];
 
-    // Ingreso de los elementos de la matriz
-    printf("Introduce los elementos de la matriz (%d x %d):\n", n, n);
+    // RF2: Llenar la matriz
+    int valor = 1;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            printf("Elemento [%d][%d]: ", i, j);
-            scanf("%d", &matriz[i][j]);
+            matriz[i][j] = valor++;
         }
     }
 
-    // Mostrar la matriz original
+    // RF3: Mostrar la matriz original
     printf("\nMatriz original:\n");
     for (int r = 0; r < n; r++) {
         for (int c = 0; c < n; c++) {
@@ -28,25 +27,25 @@ int main() {
     }
 
     int i, j;
-    // Solicitar las filas a intercambiar
-    printf("\nIntroduce los números de las dos filas a intercambiar (0 a %d): ", n - 1);
+    // RF4: Solicitar las filas a intercambiar
+    printf("\nIntroduce los numeros de las dos filas a intercambiar (0 a %d): ", n - 1);
     scanf("%d %d", &i, &j);
 
     // Validar índices
     if (i < 0 || i >= n || j < 0 || j >= n) {
-        printf("Índices fuera de rango. Terminando el programa.\n");
+        printf("Fuera de rango.\n");
         return 1;
     }
 
-    // Intercambiar las filas i y j
+    // RF5: Intercambiar las filas i y j
     for (int k = 0; k < n; k++) {
         int aux = matriz[i][k];
         matriz[i][k] = matriz[j][k];
         matriz[j][k] = aux;
     }
 
-    // Mostrar la matriz después del intercambio
-    printf("\nMatriz después de intercambiar las filas %d y %d:\n", i, j);
+    // RF6: Mostrar la matriz despues del intercambio
+    printf("\nMatriz despues de intercambiar las filas %d y %d:\n", i, j);
     for (int r = 0; r < n; r++) {
         for (int c = 0; c < n; c++) {
             printf("%4d", matriz[r][c]);
