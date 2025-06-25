45723bca7109b0713bfd26d1eb5181e8851ce21f
MarcoJoelChuquisalaGuanoluisa <mjchuquisala@espe.edu.ec>
2025-06-25 07:17:23 -0500

    Update Ejercicio1.c

--- Unidad 2/Deberes/Deber 2/Ejercicio1.c
+++ Unidad 2/Deberes/Deber 2/Ejercicio1.c
@@ -2,31 +2,24 @@
 
 int main() {
     int n;
-
-    // Solicitar el tamano de la matriz cuadrada
+    // Solicitar el tamaño de la matriz cuadrada
     printf("Ingrese el tamano de la matriz cuadrada (n x n): ");
     scanf("%d", &n);
-
     int matriz[n][n];
-
-    printf("Ingrese los elementos de la matriz desde la ultima fila y columna hasta la primera:\n");
-
-    // Entrada de la matriz en orden inverso (de abajo hacia arriba y de derecha a izquierda)
+    int contador = 1;
+    // Llenar la matriz en orden inverso
     for (int i = n - 1; i >= 0; i--) {
         for (int j = n - 1; j >= 0; j--) {
-            printf("Elemento [%d][%d]: ", i, j);
-            scanf("%d", &matriz[i][j]);
+            matriz[i][j] = contador++;
         }
     }
-
-    // Mostrar la matriz
-    printf("\nMatriz ingresada:\n");
+    // Impirimir la matriz
+    printf("\nMatriz generada automáticamente (orden inverso):\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf("%d\t", matriz[i][j]);
         }
         printf("\n");
     }
-
     return 0;
 }
