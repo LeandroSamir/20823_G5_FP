6b831d11c1af987d7ef9327b3c51c8dffa34b4c1
MarcoJoelChuquisalaGuanoluisa <mjchuquisala@espe.edu.ec>
2025-06-27 00:11:00 -0500

    Add files via upload

---
+++ Marco_Chuquisala/Operaciones y Fibonacci/operaciones.h
@@ -0,0 +1,10 @@
+#ifndef OPERACIONES_H_INCLUDED
+#define OPERACIONES_H_INCLUDED
+
+float sumar(float a, float b);
+float restar(float a, float b);
+float multiplicar(float a, float b);
+float dividir(float a, float b);
+void fibonacci(int n);
+
+#endif // OPERACIONES_H_INCLUDED
